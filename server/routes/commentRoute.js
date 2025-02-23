b5063a006a1cf60ae1a5e36629d323f2b5987809
Thales Muller <thalesmuller@outlook.com.br>
2025-02-22 22:33:13 -0300

    [BUGFIX] Fixing controller methods

--- server/routes/commentRoute.js
+++ server/routes/commentRoute.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const commentController = require('../controllers/commentController');
 
-router.get('/', commentController.getAllUsers);
-router.get('/:id', commentController.findUserById);
-router.post('/', commentController.createUser);
-router.patch('/:id', commentController.updateUser);
-router.delete('/:id', commentController.deleteUser);
+router.get('/', commentController.getAllComments);
+router.get('/:id', commentController.findCommentById);
+router.post('/', commentController.createComment);
+router.patch('/:id', commentController.updateComment);
+router.delete('/:id', commentController.deleteComment);
 
 module.exports = router;
